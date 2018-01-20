dd7e30ece08d4ae532772852d3b27a92b8561cc1
Tim Kharshan <timkharshan@hotmail.com>
2018-01-20 15:31:57 -0800

    basic view

--- public/java.js
+++ public/java.js
@@ -1,2 +1,12 @@
-console.log('here');
-window.boom = 'boom'
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('loaded');
+    const subBtn = document.getElementById('submit');
+    const input = document.getElementById('chatin');
+
+    subBtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('submit clicked');
+        console.log(e);
+        console.log(input.value)
+    })
+});
