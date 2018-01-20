bc57cad33b18a9da9cdc63fae22820f725a8856b
Tim Kharshan <timkharshan@hotmail.com>
2018-01-20 12:26:48 -0800

    heroku

--- server.rb
+++ server.rb
@@ -2,11 +2,10 @@ require 'socket'
 require 'thread'
 include Socket::Constants
 ESCAPE_CHAR = 'q'
-ENV["port"] = '9999'
 socket = Socket.new(AF_INET, SOCK_STREAM, 0)
 # pack_sockaddr_in(80, 'example.com')
 p ENV
-sockaddress = Socket.pack_sockaddr_in(ARGV[0] || ENV["port"] || 9876, ARGV[1] || ENV["address"] || 'localhost')
+sockaddress = Socket.pack_sockaddr_in(ARGV[0] || ENV["PORT"] || 9876, ARGV[1] || ENV["address"] || 'localhost')
 socket.bind(sockaddress)
 listen = socket.listen(5)
 
