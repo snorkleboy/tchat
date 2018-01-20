2f99888ce6a5912e5afa3a8780194b189e39739d
Tim Kharshan <timkharshan@hotmail.com>
2018-01-20 12:11:57 -0800

    uploaded to heroku

---
+++ middlewares/backend.rb
@@ -0,0 +1,38 @@
+require 'faye/websocket'
+
+module ChatDemo
+  class ChatBackend
+    KEEPALIVE_TIME = 15 # in seconds
+
+    def initialize(app)
+      @app     = app
+      @clients = []
+    end
+
+    def call(env)
+        if Faye::WebSocket.websocket?(env)
+            ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
+
+            ws.on :open do |event|
+                p ['websocket connection opened', ws.object_id]
+                @clients << ws
+            end
+
+            ws.on :message do |event|
+                p ['websocket message event', event.data]
+                @clients.each {|client| client.send(event.data) }
+            end
+
+            ws.on :close do |event|
+                p ['websocket closing', ws.object_id, event.code, event.reason]
+                @clients.delete(ws)
+                ws = nil
+            end
+
+            ws.rack_response
+        else
+            @app.call(env)
+        end
+    end
+  end
+end
