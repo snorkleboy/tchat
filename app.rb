6f3e99b4e34736cb5c07077b305132a4a5eb8bc9
Tim Kharshan <timkharshan@hotmail.com>
2018-01-20 13:56:12 -0800

    semi rack

--- app.rb
+++ app.rb
@@ -1,3 +1,3 @@
 app = Proc.new do |env|
-  ['200', {'Content-Type' => 'text/html'}, ["Hello world! The time is #{Time.now}"]]
+  ['200', {'Content-Type' => 'text/html'}, ["Hello world! this is a 'rack' app and The time is #{Time.now}"]]
 end
