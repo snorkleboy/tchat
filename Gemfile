bad79fdda214df8d7a5973f0abbb6018bdccf7f7
Artem Kharshan <timkharshan@hotmail.com>
2018-02-03 20:54:28 -0800

    connected to postgresql I think

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'activerecord'
 
+gem 'sequel'
 gem 'rack'
 gem 'rake'
 gem 'pg'
--- middlewares/controllers/apiController.rb
+++ middlewares/controllers/apiController.rb
@@ -1,10 +1,14 @@
 require 'rack'
 require 'json'
-
+require 'sequel'
 module Chat
     class APIController
         def initialize(app)
             @app=app
+            db = Sequel.connect(adapter: :postgres, user: 'snorkleboy', host: '/var/run/postgresql', port: 5432,
+  database: 'chat', max_connections: 10)
+            p db
+
 
         end
         def call(env)
